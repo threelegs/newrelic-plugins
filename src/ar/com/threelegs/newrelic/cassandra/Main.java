e28a44c34eeb74a6656191d9bc097dc08ec8a072
Juan Manuel Formoso <jformoso@gmail.com>
2013-07-03 12:08:05 -0300

    added configuration for turning on and off the different 'focused plugins'

--- src/ar/com/threelegs/newrelic/cassandra/Main.java
+++ src/ar/com/threelegs/newrelic/cassandra/Main.java
@@ -18,15 +18,24 @@ public class Main {
 		Runner runner = new Runner();
 
 		Config config = ConfigFactory.parseFile(new File("config/application.conf"));
+		Config activation = ConfigFactory.parseFile(new File("config/activation.conf"));
 
 		for (Config c : config.getConfigList("cassandra")) {
-			runner.register(new CassandraRing(c));
+			if (activation.getBoolean("ring")) {
+				runner.register(new CassandraRing(c));
+			}
 
-			List<String> ringHosts = CassandraHelper.getRingHosts(c.getString("discovery_host"), c.getString("jmx_port"));
-			for (final String host : ringHosts) {
-				runner.register(new CassandraHost(host, c));
-				// TODO: Temp test
-				runner.register(new CassandraData(host, c));
+			if (activation.getBoolean("host") || activation.getBoolean("data")) {
+				List<String> ringHosts = CassandraHelper.getRingHosts(c.getString("discovery_host"), c.getString("jmx_port"));
+				for (final String host : ringHosts) {
+					if (activation.getBoolean("host")) {
+						runner.register(new CassandraHost(host, c));
+					}
+					// TODO: Temp test
+					if (activation.getBoolean("data")) {
+						runner.register(new CassandraData(host, c));
+					}
+				}
 			}
 		}
 
