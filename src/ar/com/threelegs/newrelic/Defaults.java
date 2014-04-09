e9cad40d5f951eca44dbb416f284f10a450af1d2
Juan Manuel Formoso <jformoso@gmail.com>
2014-04-09 16:12:11 -0300

    fixed plugin name defaults

--- src/ar/com/threelegs/newrelic/Defaults.java
+++ src/ar/com/threelegs/newrelic/Defaults.java
@@ -2,7 +2,7 @@ package ar.com.threelegs.newrelic;
 
 public class Defaults {
 	public static String CASSANDRA_PLUGIN_NAME = "ar.com.3legs.newrelic.cassandra";
-	public static String VARNISH_PLUGIN_NAME = "ar.com.3legs.newrelic.cassandra";
-	public static String JMXREMOTE_PLUGIN_NAME = "ar.com.3legs.newrelic.cassandra";
+	public static String VARNISH_PLUGIN_NAME = "ar.com.3legs.newrelic.varnish";
+	public static String JMXREMOTE_PLUGIN_NAME = "ar.com.3legs.newrelic.jmxremote";
 	public static String VERSION = "2.0.0";
 }
